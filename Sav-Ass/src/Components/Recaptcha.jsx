f9399dbac28806ab5f0be493a69964629ddbb8ae
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2024-12-03 20:25:13 +0530

    updated

--- Sav-Ass/src/Components/Recaptcha.jsx
+++ Sav-Ass/src/Components/Recaptcha.jsx
@@ -5,7 +5,7 @@ const ReCaptchaComponent = ({ onCaptchaChange }) => {
    return (
       <div className="recaptcha-container">
          <ReCAPTCHA
-            sitekey="6LfuS5EqAAAAABcd3Ao7oPBsZCzNpzLz6X4z_iGI" 
+            sitekey="6LcATpEqAAAAABWO9S_D0g31-l1_KGZIr6dFXKqW" 
             onChange={onCaptchaChange}
          />
       </div>
