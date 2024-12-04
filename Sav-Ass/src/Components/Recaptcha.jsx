2c84cd748c6c6cf11c0ad09bddad8a46da24d13b
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2024-12-04 12:32:02 +0530

    updated

--- Sav-Ass/src/Components/Recaptcha.jsx
+++ Sav-Ass/src/Components/Recaptcha.jsx
@@ -5,7 +5,10 @@ const ReCaptchaComponent = ({ onCaptchaChange }) => {
    return (
       <div className="recaptcha-container">
          <ReCAPTCHA
-            sitekey="6LcATpEqAAAAABWO9S_D0g31-l1_KGZIr6dFXKqW" 
+
+         // for domain site key 6Ldr7ZEqAAAAAFwBekmDiqmUG6_-hJrtJphwXl3K
+         // 6LcATpEqAAAAABWO9S_D0g31-l1_KGZIr6dFXKqW local host
+            sitekey="6Ldr7ZEqAAAAAFwBekmDiqmUG6_-hJrtJphwXl3K"
             onChange={onCaptchaChange}
          />
       </div>
